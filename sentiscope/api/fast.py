9472f6c1acab33f45b39cd964966c94295979b04
Johannes Blass <jojo.blass@gmail.com>
2025-06-16 14:21:42 +0200

    Documentation and refactoring

--- sentiscope/api/fast.py
+++ sentiscope/api/fast.py
@@ -1,18 +1,21 @@
 #API imports
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import numpy as np
 
 #Preprocessing
-from sentiscope.ml_logic.preprocessor import preprocess_ml, load_pipeline, \
-    load_tokenizer, preprocess_dl
+from sentiscope.ml_logic.preprocessor import load_pipeline, \
+    load_tokenizer
 from sentiscope.ml_logic.utils import preprocess_series
 
 #Prediction
-from sentiscope.ml_logic.model import predict, load_ml_model, load_dl_model
+from sentiscope.ml_logic.model import load_ml_model, load_dl_model
 
 #Post request import
 from pydantic import BaseModel
+
+#imports for preprocessing, predictions and visualization parameters
+from sentiscope.ml_logic.explain import \
+    create_predict_fn, explain_with_lime, explain_ml
 
 #Initialize FastAPI
 app = FastAPI()
@@ -47,29 +50,14 @@ def bert_predict(review):
     http://127.0.0.1:8000/bert?review=This+is+a+bad+review
     """
 
-    #Tokenize input
-    X_pred = preprocess_dl(X=review, tokenizer=app.state.tokenizer)
+    #Define function needed to get bert prediction and analysis
+    predict_proba_fn = \
+        create_predict_fn(app.state.model_dl, app.state.tokenizer)
 
-    #Get a sentiment prediction
-    prediction = app.state.model_dl.predict(X_pred)
+    #Get sentiment and visualization params
+    explanation = explain_with_lime(review, predict_proba_fn)
 
-    #Get the actual label out of the predictions
-    logits = prediction.logits
-    y_pred = np.argmax(logits, axis=1).tolist()
-
-    #Convert class to natural language
-    if y_pred[0] == 0:
-        return {
-            "Sentiment" : "Negative"
-        }
-    elif y_pred[0] == 1:
-        return{
-            "Sentiment" : "Positive"
-        }
-    else:
-        return{
-            "Sentiment" : "No output"
-        }
+    return explanation
 
 
 #Define /predict endpoint
@@ -83,98 +71,21 @@ def predict_sentiment(review):
     http://127.0.0.1:8000/predict?review=This+is+a+bad+review
     """
 
-    #Preprocess the review
-    X_pred = preprocess_ml(review,app.state.pipeline)
-
-    #Predict the sentiment of the review
-    prediction = predict(X_pred, app.state.model_ml)
-
-    #Extract vectorizer for visualization data
-    vectorizer = app.state.pipeline['vectorizer']
-
-    #Extract data from model and vectorizer
-    coefs = app.state.model_ml.coef_[0]
-    feature_names = vectorizer.get_feature_names_out()
-    input_indices = X_pred.nonzero()[1]
-    tfidf_values = X_pred.toarray()[0][input_indices]
-    input_tokens = [feature_names[i] for i in input_indices]
-    word_coefs = coefs[input_indices]
-
-    # Compute word contributions
-    contributions = tfidf_values * word_coefs
-    contrib_dict = dict(zip(input_tokens, contributions))
-
-    # Sort contributions to find top positives and negatives
-    sorted_items = sorted(contrib_dict.items(), key=lambda x: x[1])
-    top_negative = [w for w, _ in sorted_items[:2]]
-    top_positive = [w for w, _ in sorted_items[-2:]]
-
-    #Turn predicted label to readable text and also return vis. data
-    if prediction == -1:
-        return {
-            "Sentiment": "Negative",
-            "contributions": contrib_dict,
-            "top_positive": top_positive,
-            "top_negative": top_negative
-            }
-    elif prediction == 0:
-        return {
-            "Sentiment": "Positive",
-            "contributions": contrib_dict,
-            "top_positive": top_positive,
-            "top_negative": top_negative
-            }
-    else:
-        return {"Sentiment": "No output"}
+    #Call function to preprocess and predict review
+    return explain_ml(review=review, \
+        model = app.state.model_ml, pipeline=app.state.pipeline)
 
 
-#TODO:finish this endpoint
 @app.post("/text")
 def receive_text(my_text: Text):
-    body = my_text.text
+    """
+    Endpoint to make a prediction via POST.
+    """
+    review = my_text.text
 
-    #Preprocess the review
-    X_pred = preprocess_ml(body,app.state.pipeline)
+    return explain_ml(review=review, \
+        model = app.state.model_ml, pipeline=app.state.pipeline)
 
-    #Predict the sentiment of the review
-    prediction = predict(X_pred, app.state.model)
-
-    vectorizer = app.state.pipeline['vectorizer']
-
-    coefs = app.state.model_ml.coef_[0]
-    feature_names = vectorizer.get_feature_names_out()
-
-    input_indices = X_pred.nonzero()[1]
-    tfidf_values = X_pred.toarray()[0][input_indices]
-    input_tokens = [feature_names[i] for i in input_indices]
-    word_coefs = coefs[input_indices]
-
-    # Compute word contributions
-    contributions = tfidf_values * word_coefs
-    contrib_dict = dict(zip(input_tokens, contributions))
-
-    # Sort contributions to find top positives and negatives
-    sorted_items = sorted(contrib_dict.items(), key=lambda x: x[1])
-    top_negative = [w for w, _ in sorted_items[:2]]
-    top_positive = [w for w, _ in sorted_items[-2:]]
-
-    #Turn predicted label to readable text
-    if prediction == -1:
-        return {
-            "Sentiment": "Negative",
-            "contributions": contrib_dict,
-            "top_positive": top_positive,
-            "top_negative": top_negative
-            }
-    elif prediction == 0:
-        return {
-            "Sentiment": "Positive",
-            "contributions": contrib_dict,
-            "top_positive": top_positive,
-            "top_negative": top_negative
-            }
-    else:
-        return {"Sentiment": "No output"}
 
 #Root endpoint
 @app.get("/")
--- sentiscope/ml_logic/explain.py
+++ sentiscope/ml_logic/explain.py
@@ -1,30 +1,61 @@
+#DL explaining
 from lime.lime_text import LimeTextExplainer
-import tensorflow as tf
-import numpy as np
 
+#Required imports
+import tensorflow as tf
+
+#Preprocessing and prediction
+from sentiscope.ml_logic.model import predict
+from sentiscope.ml_logic.preprocessor import preprocess_ml, preprocess_dl
+
+#Variables for Lime
 class_names = ["Negative", "Positive"]
 
+
 def create_predict_fn(model, tokenizer):
+    """
+    Function to create a prediction function which can be called by the API
+    directly
+    """
 
     @tf.function
-    def predict_logits(inputs):
-        return model(inputs, training=False).logits
+    def predict_logits(tokens):
+        """
+        Optimized function to create a prediction of the trained bert model
 
-    def predict_proba(texts):
-        tokenized = tokenizer(
-            texts,
-            return_tensors="tf",
-            padding="max_length",
-            truncation=True,
-            max_length=400
-        )
+        Input: TF Tensor
+        Output: TF prediction logits
+        """
+        return model(tokens, training=False).logits
+
+    def predict_proba(review):
+        """
+        Function which preprocesses user input, calls predict function and turns
+        the returned values to probabilities of the review belonging to either
+        the positive or negative class using softmax.
+
+        Input: review string
+        Output: numpy array of 2 probabilities
+        """
+
+        #Preprocess input and fetch prediction
+        tokenized = preprocess_dl(review,tokenizer)
         logits = predict_logits(tokenized)
+
+        #Calculate probas with softmax
         probs = tf.nn.softmax(logits, axis=1)
         return probs.numpy()
 
+    #Return function so API can call it
     return predict_proba
 
 def explain_with_lime(text, predict_proba_fn, num_features=10):
+    """
+    Function which tracks which parts of a review had the most positive or
+    negative influence on the prediction of the model
+    """
+
+    #Calculate contributions of each word of the review
     explainer = LimeTextExplainer(class_names=class_names)
     exp = explainer.explain_instance(
         text_instance=text,
@@ -33,14 +64,67 @@ def explain_with_lime(text, predict_proba_fn, num_features=10):
         labels=[1],
         num_samples=300
     )
+
+    #Sort contributions and extract the words which have the most impact
+    # positively and negatively
     contribs = dict(exp.as_list(label=1))
     sorted_contribs = sorted(contribs.items(), key=lambda x: x[1])
     top_negative = [w for w, score in sorted_contribs if score < 0][:2]
-    top_positive = [w for w, score in reversed(sorted_contribs) if score > 0][:2]
-    sentiment = "Positive" if predict_proba_fn([text])[0][1] > 0.5 else "Negative"
+    top_positive = \
+        [w for w, score in reversed(sorted_contribs) if score > 0][:2]
+
+    #Return sentiment and calculated contributions + top positive/negative words
+    sentiment = "Positive" if \
+        predict_proba_fn([text])[0][1] > 0.5 else "Negative"
     return {
         "sentiment": sentiment,
         "contributions": contribs,
         "top_positive": top_positive,
         "top_negative": top_negative
     }
+
+
+def explain_ml(review,model, pipeline):
+    #Preprocess the review
+    X_pred = preprocess_ml(review,pipeline)
+
+    #Predict the sentiment of the review
+    prediction = predict(X_pred, model)
+
+    #Extract vectorizer for visualization data
+    vectorizer = pipeline['vectorizer']
+
+    #Extract data from model and vectorizer
+    coefs = model.coef_[0]
+    feature_names = vectorizer.get_feature_names_out()
+    input_indices = X_pred.nonzero()[1]
+    tfidf_values = X_pred.toarray()[0][input_indices]
+    input_tokens = [feature_names[i] for i in input_indices]
+    word_coefs = coefs[input_indices]
+
+    # Compute word contributions
+    contributions = tfidf_values * word_coefs
+    contrib_dict = dict(zip(input_tokens, contributions))
+
+    # Sort contributions to find top positives and negatives
+    sorted_items = sorted(contrib_dict.items(), key=lambda x: x[1])
+    top_negative = [w for w, _ in sorted_items[:2]]
+    top_positive = [w for w, _ in sorted_items[-2:]]
+
+    #Turn predicted label to readable text and also return vis. data
+    if prediction == -1:
+        return {
+            "Sentiment": "Negative",
+            "contributions": contrib_dict,
+            "top_positive": top_positive,
+            "top_negative": top_negative
+            }
+    elif prediction == 0:
+        return {
+            "Sentiment": "Positive",
+            "contributions": contrib_dict,
+            "top_positive": top_positive,
+            "top_negative": top_negative
+            }
+    else:
+        return {"Sentiment": "No output"}
